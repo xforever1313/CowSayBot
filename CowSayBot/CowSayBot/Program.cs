9e8dc0c7320cd919f820d89536e400ef68a5bfb9
Seth Hendrick <seth@shendrick.net>
2016-04-24 00:08:11 -0400

    The CowSayBot is now a plugin that can be used in the GenericIrcBot framework.

--- CowSayBot/CowSayBot/Program.cs
+++ CowSayBot/CowSayBot/Program.cs
@@ -5,43 +5,28 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 using System;
-using GenericIrcBot;
 using System.Collections.Generic;
-using System.IO;
-using System.Diagnostics;
-using System.Text.RegularExpressions;
 using System.Threading;
 using Mono.Unix;
 using Mono.Unix.Native;
+using GenericIrcBot;
+using CowSayBotPlugin;
 
-namespace CowSayBot
+namespace CowSayBotMain
 {
     class MainClass
     {
         // -------- Fields --------
 
-        private const string nick = "CowSayBot";
-
         /// <summary>
-        /// Regex to look for while in IRC.
+        /// Nick name to use.
         /// </summary>
-        private const string cowsayRegex = 
-            @"!(?<cmd>(" +
-            defaultCommand + ")|(" +
-            tuxCommand + @")|(" +
-            vaderCommand + @")|(" +
-            mooseCommand + @")|(" +
-            lionCommand + @"))\s+(?<cowsayMsg>.+)";
-
-        /// <summary>
-        /// Path to the cowsay binary.
-        /// </summary>
-        private static readonly string cowsayProgram = "/usr/bin/cowsay";
+        private const string nick = "CowSayBot2";
 
         /// <summary>
         /// The event to wait on until we exit.
         /// </summary>
-        private static ManualResetEvent exitEvent = new ManualResetEvent( false );
+        private static readonly ManualResetEvent exitEvent = new ManualResetEvent( false );
 
         /// <summary>
         /// Signals to watch for to terminate teh program.
@@ -50,52 +35,6 @@ namespace CowSayBot
             new UnixSignal( Signum.SIGTERM ), // Termination Signal
             new UnixSignal( Signum.SIGINT )   // Interrupt from the keyboard
         };
-
-        /// <summary>
-        /// Process Start info
-        /// </summary>
-        private static readonly ProcessStartInfo cowSayInfo;
-
-        // ---- Commands ----
-
-        /// <summary>
-        /// Command the user uses to use the default cow.
-        /// </summary>
-        private const string defaultCommand = "cowsay";
-
-        /// <summary>
-        /// Command user uses to have tux appear.
-        /// </summary>
-        private const string tuxCommand = "tuxsay";
-
-        /// <summary>
-        /// Command user uses to have vader appear.
-        /// </summary>
-        private const string vaderCommand = "vadersay";
-
-        /// <summary>
-        /// Command user uses to have a moose appear.
-        /// </summary>
-        private const string mooseCommand = "moosesay";
-
-        /// <summary>
-        /// Command user uses to have a lion appear.
-        /// </summary>
-        private const string lionCommand = "lionsay";
-
-        // -------- Constructor --------
-
-        /// <summary>
-        /// Static Constructor.
-        /// </summary>
-        static MainClass()
-        {
-            cowSayInfo = new ProcessStartInfo();
-            cowSayInfo.RedirectStandardInput = true;
-            cowSayInfo.RedirectStandardOutput = true;
-            cowSayInfo.UseShellExecute = false;
-            cowSayInfo.FileName = cowsayProgram;
-        }
 
         // -------- Functions --------
 
@@ -106,13 +45,6 @@ namespace CowSayBot
         /// <returns>The exit code that is given to the operating system after the program ends.</returns>
         public static int Main( string[] args )
         {
-            if( File.Exists( cowsayProgram ) == false )
-            {
-
-                Console.WriteLine( "Cowsay not installed in " + cowsayProgram + ".  Aborting." );
-                return 1;
-            }
-
             // Start signal detection thread.
             // Once we get a CTRL+C or a sigterm, we will abort the program.
             Thread signalThread =
@@ -133,19 +65,19 @@ namespace CowSayBot
             config.RealName = "Cow Say Bot";
             config.UserName = nick;
 
-            // Generate configs.
-
+            // Initialize Plugins
             List<IIrcHandler> configs = new List<IIrcHandler>();
 
-            IIrcHandler cowSayConfig = 
-                new MessageHandler(
-                    cowsayRegex,
-                    HandleCowsayCommand,
-                    5,
-                    ResponseOptions.RespondOnlyToChannel
-                );
+            // Load cow-say plugin.
+            CowSayBot cowSayPlugin = new CowSayBot();
+            string error;
+            if( cowSayPlugin.Validate( out error ) == false )
+            {
+                Console.WriteLine( error );
+                return 1;
+            }
 
-            configs.Add( cowSayConfig );
+            configs.AddRange( cowSayPlugin.GetHandlers() );
 
             // Must handle pings.
             configs.Add( new PingHandler() );
@@ -158,93 +90,6 @@ namespace CowSayBot
             }
 
             return 0;
-        }
-
-        /// <summary>
-        /// Handles the cowsay command.
-        /// </summary>
-        /// <param name="writer">The IRC Writer to write to.</param>
-        /// <param name="response">The response from the channel.</param>
-        private static void HandleCowsayCommand( IIrcWriter writer, IrcResponse response )
-        {
-            try
-            {
-                Match cowMatch = Regex.Match( response.Message, cowsayRegex );
-                if( cowMatch.Success )
-                {
-                    string messageToCowsay = cowMatch.Groups["cowsayMsg"].Value;
-
-                    // Run the cowsay subprocess.
-                    cowSayInfo.Arguments = GetArguments( cowMatch.Groups["cmd"].Value );
-
-                    string cowSayedMessage = string.Empty;
-                    using( Process cowsayProc = Process.Start( cowSayInfo ) )
-                    {
-                        using( StreamReader stdout = cowsayProc.StandardOutput )
-                        {
-                            using( StreamWriter stdin = cowsayProc.StandardInput )
-                            {
-                                stdin.Write( messageToCowsay );
-                                stdin.Flush();
-                            }
-
-                            cowSayedMessage = stdout.ReadToEnd();
-                        }
-
-                        // If we hang for more than 15 seconds, abort.
-                        if( cowsayProc.WaitForExit( 15 * 1000 ) == false )
-                        {
-                            cowsayProc.Kill();
-                        }
-                    }
-
-                    if( string.IsNullOrEmpty( cowSayedMessage ) == false )
-                    {
-                        writer.SendCommand( cowSayedMessage );
-                    }
-                }
-                else
-                {
-                    Console.Error.WriteLine( "Saw unknown line:" + response.Message );
-                }
-            }
-            catch( Exception e )
-            {
-                Console.Error.WriteLine( "*********************" );
-                Console.Error.WriteLine( "Caught Exception:" );
-                Console.Error.WriteLine( e.Message );
-                Console.Error.WriteLine( e.StackTrace );
-                Console.Error.WriteLine( "**********************" );
-            }
-        }
-
-        /// <summary>
-        /// Gets the arguments based on the command the user gave us.
-        /// </summary>
-        /// <param name="commandString">The command string the user gave.  e.g. !tuxsay</param>
-        /// <returns>The arguments.</returns>
-        private static string GetArguments( string commandString )
-        {
-            switch( commandString )
-            {
-                case defaultCommand:
-                    return string.Empty;
-
-                case tuxCommand:
-                    return "-f tux";
-
-                case vaderCommand:
-                    return "-f vader";
-
-                case mooseCommand:
-                    return "-f moose";
-
-                case lionCommand:
-                    return "-f moofasa";
-
-                default:
-                    return string.Empty;
-            }
         }
     }
 }
